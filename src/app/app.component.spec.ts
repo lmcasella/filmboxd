550d4c44bdfe809f90b56b4e12d5cb3a7e321e7c
lmcasella <lautaromcasella@gmail.com>
2022-12-21 20:30:42 -0300

    Renombrar proyecto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'filmbox'`, () => {
+  it(`should have as title 'filmboxd'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('filmbox');
+    expect(app.title).toEqual('filmboxd');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('filmbox app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('filmboxd app is running!');
   });
 });
