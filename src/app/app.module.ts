9f52bb3cbcb82273cb07cef5edfb0f97a53b1bb3
lmcasella <lautaromcasella@gmail.com>
2022-12-21 15:09:11 -0300

    Agregar estructura de header y body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
