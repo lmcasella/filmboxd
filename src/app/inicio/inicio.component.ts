00d5ea0bfd84a225865b4a38104efd4d8ad851f8
lmcasella <lautaromcasella@gmail.com>
2022-12-21 19:58:15 -0300

    Migrar de CSS a SCSS

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.scss']
 })
 export class InicioComponent {
 
